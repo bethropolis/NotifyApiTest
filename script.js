c74c0760241f907d414446cc261d0c9630af1bcd
Bethuel <bethropolis@gmail.com>
2021-04-22 08:21:58 +0000

    finalized the code

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let current = false; 
+let current = true;
 let message = "this is a test notification message ";
-let title = "title"; 
+let title = "title";
 
 function notifyMe() {
   // Let's check if the browser supports notifications
@@ -10,48 +10,94 @@ function notifyMe() {
   // Let's check whether notification permissions have already been granted
   else if (Notification.permission === "granted") {
     // If it's okay let's create a notification
-    const greeting = new Notification(title,{
-  body: message, 
-  icon: './img/goodday.png'
-});
- 
+    sendNotification();
   }
 
   // Otherwise, we need to ask the user for permission
   else if (Notification.permission !== "denied") {
     Notification.requestPermission().then(function (permission) {
+      $('#notify').attr('title', `please click allow for the website to send you notifications`);
       // If the user accepts, let's create a notification
-      if (permission === "granted") {        
-        var notification = new Notification("Hi there!");
-          $('#notify').text("notified"); 
+      if (permission === "granted") {
+        sendNotification();
       }
     });
-  }else{
-    alert(":( denied to send notification"); 
-  } 
-}
-function showOne(data){
-  if (data){
-    $('#notify').hide()
-     $('.data').show("slow");
-  }else{
-     $('.data').hide(1000);
-     $('#notify').show('slow');
+  } else {
+    alert(":( denied to send notification");
+    $('#notify').text("denied");
+    $('#notify').css('background', '#FF8066');
+    $('#notify').attr('disabled', true);
+    $('#notify').attr('title', `please allow the website to send you notifications`);
+  }
+
+  function sendNotification() {
+    const greeting = new Notification(title, {
+      body: message,
+      icon: './icon.png'
+    });
+    greeting.addEventListener('click', function () {
+      window.open('https://github.com/bethropolis/NotifyApiTest');
+    });
+
+    $('#notify').text("notified");
+    $('#notify').css('background', 'rgb(19, 196, 155) none repeat scroll 0% 0%');
+    $('#notify').attr('title', `notification sent`);
   }
 }
-$("#feedback").click(function(){
-  var fdbck = prompt("enter your feedback to developer";)
-    alert("feedback successfully sent");
-})
-$("#edit").click(function(){
-  showOne(!current) 
-  current = !current; 
-})
+function showOne(data) {
+  if (data) {
+    $('#notify').hide();
+    $('.data').show("slow");
+  } else {
+    $('.data').hide(1000);
+    $('#notify').show('slow');
+  }
+  current = !current;
+}
 
-$("#save").click(function(){
-  $("#save").attr('')
-})
-$("#notify").click(function(){
-    $('#notify').text("notifying") 
-   notifyMe();    
-}) 
+$("#feedback").click(function () {
+  var fdbck = prompt("enter your feedback to the developer");
+  if (fdbck) {
+    try{ 
+    $.post('https://pushme.vercel.app/api/sendNotification', {
+      ContentType: "application/x-www-form-urlencoded",
+      code: "5ro94qxv1jv",
+      title: "feedback",
+      message: fdbck
+    }).done(function( data ) { 
+    if (data.success) alert("feedback successfully sent"); 
+  });  
+   }catch(e){
+       alert("there was an error sending feedback");
+        console.error(e.message);  
+    }
+  } else {
+    alert("feedback arborted");
+  }
+});
+$("#edit").click(function () {
+  showOne(current);
+});
+
+$("#title").on("input", function () {
+  title = $("#title").val();
+  $("#save").attr('disabled', false);
+});
+
+$("#message").on("input", function () {
+  message = $("#message").val();
+  $("#save").attr('disabled', false);
+});
+
+$("#save").click(function () {
+  $("#save").attr('disabled', true);
+  title = $("#title").val();
+  message = $("#message").val();
+  showOne(current);
+});
+
+$("#notify").click(function () {
+  $('#notify').text("notifying...");
+  $('#notify').css('background', 'hsl(245, 84%, 68%)');
+  notifyMe();
+}); 
